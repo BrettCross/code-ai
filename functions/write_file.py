c96ae8054b07792ebd0d794e6d977b242326c0a5
Brett Cross <9117646+BrettCross@users.noreply.github.com>
2025-06-23 10:16:54 -0400

    add writing to file and running python code functinoality

--- functions/write_file.py
+++ functions/write_file.py
@@ -6,8 +6,6 @@ def write_file(working_directory, file_path, content):
     abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
     if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
-    print(f"abs_file_path={abs_file_path}")
-    print(f"exists={os.path.exists(abs_file_path)}")
     if not os.path.exists(abs_file_path):
         try:
             os.makedirs(os.path.dirname(abs_file_path), exist_ok=True)
