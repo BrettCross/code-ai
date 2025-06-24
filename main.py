7b77fa146d4d7f6cad0ea029dd9150331c4a0a0f
Brett Cross <9117646+BrettCross@users.noreply.github.com>
2025-06-24 11:39:56 -0400

    allow AI to call functions

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from google.genai import types
 from dotenv import load_dotenv
 
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 
 def main():
@@ -52,7 +52,16 @@ def generate_content(client, messages, verbose):
         return
     
     for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        # print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        function_call_result = call_function(function_call_part, verbose=verbose)
+        try:
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+        except Exception as e:
+            return (
+                f"Error: No response from calling function: {function_call_part.name}({function_call_part.args})"
+            )
+
 
 if __name__ == "__main__":
     main()
